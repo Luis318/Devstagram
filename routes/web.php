099ea712eb1b372732d7127974d83254d117a093
luis318 <mg17032@ues.edu.sv>
2023-01-31 16:05:10 -0600

    formularios de registro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('principal');
 });
+
+Route::get('/crear-cuenta',[RegisterController::class, 'index']);
+Route::post('/crear-cuenta',[RegisterController::class, 'store']);
