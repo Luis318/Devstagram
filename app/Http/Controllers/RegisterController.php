4154f4d0f4e1b0d2804fbf212ab09bfcb067030d
luis318 <mg17032@ues.edu.sv>
2023-02-01 17:05:41 -0600

    migraciones y base de datos

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,7 +10,17 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(){
-        dd('post...');
+    public function store(Request $request){
+        //dd($request);
+
+        //validaciones
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'username' => 'required|unique:users|min:3|max:30',
+            'email' => 'required|unique:users|email|max:70',
+            'password' => 'required|min:8|max:20|confirmed',
+        ]);
+
+        dd('creando usuario');
     }
 }
